885e4a41e07dc920ec6d12c733ff82a6f3ba67b1
mynameistaehyun <157575199+mynameistaehyun@users.noreply.github.com>
2024-02-17 12:23:53 +0900

    Update views.py

--- agmo_myfarm/workdiary/views.py
+++ agmo_myfarm/workdiary/views.py
@@ -4,16 +4,13 @@ from work.models import Works
 from django.http import JsonResponse
 from django.utils import timezone
 from farm.models import FarmField
-
+from .models import WorkDiary
 
 
 def main(request):
     today_day = timezone.now().day
-
-    farm_list = FarmField.objects.filter(field_name__isnull=False)
     content={
     'today_day': today_day,
-    'farm_field':farm_list
     }
 
     return render(request, 'workdiary/workdiary_main.html', content)
@@ -24,18 +21,22 @@ def work_info_view(request):
     if request.method == 'GET':
         date = request.GET.get('date')  # 클라이언트에서 전달된 날짜를 가져옴
 
-
         try:
-            work_records = Works.objects.filter(start_date_day__lte=date, end_date_day__gte=date)
+            work_records = Works.objects.all()
             data = []
             for work_record in work_records:
-                data.append({
-                    'date': date,
-                    'field_name': work_record.work_fields.field_name,
-                    'work_name': work_record.work_name,
-                    'crop': work_record.work_fields.crop,
-                    'user_memo': work_record.user_memo,
-                })
+                start_date_day = int(work_record.start_date_day)
+                end_date_day = int(work_record.end_date_day)
+                query_date = int(date)
+                if start_date_day <= query_date <= end_date_day:
+                    data.append({
+                        'date': date,
+                        'machine_name':work_record.machine_name,
+                        'field_name': work_record.work_fields.field_name,
+                        'work_name': work_record.work_name,
+                        'crop': work_record.work_fields.crop,
+                        'user_memo': work_record.user_memo,
+                    })
             return JsonResponse(data, safe=False)
         except Works.DoesNotExist:
             return JsonResponse({'error': '작업 정보를 찾을 수 없습니다.'}, status=404)
